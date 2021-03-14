bc95c24d02c7157a9500bd42830bac3eb050f059
Dbasilio-dev <dbasiliodev@gmail.com>
2021-03-14 19:16:59 -0300

    Sqlite3 foi removido do projeto e mongoDB foi adicionado. Rotas foram finalizadas.

--- src/routes/costumers.js
+++ src/routes/costumers.js
@@ -1,5 +1,83 @@
-module.exports = (app) => {
-  app.get("/costumers", (req, res) => {
-    res.send("route costumers up");
+const express = require("express");
+const router = express.Router();
+const User = require("../models/user");
+
+router.get("/", async (req, res) => {
+  try {
+    const costumers = await User.find({ type: "Cliente" });
+    res.status(201).json(costumers);
+  } catch (err) {
+    res.status(500).json({ message: err });
+  }
+});
+
+router.get("/:id", getUsers, (req, res) => {
+  res.send(res.user);
+});
+
+router.post("/", async (req, res) => {
+  const user = new User({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    type: req.body.type,
+    plan: req.body.plan,
+    started: req.body.started,
   });
-};
+  try {
+    const newUser = await user.save();
+    res.status(201).json(newUser);
+  } catch (err) {
+    res.status(500).json({ message: err });
+  }
+});
+
+router.put("/:id", getUsers, async (req, res) => {
+  if (req.body.name != null) {
+    res.user.name = req.body.name;
+  }
+  if (req.body.email != null) {
+    res.user.email = req.body.email;
+  }
+  if (req.body.password != null) {
+    res.user.password = req.body.password;
+  }
+  if (req.body.type != null) {
+    res.user.type = req.body.type;
+  }
+  if (req.body.plan != null) {
+    res.user.plan = req.body.plan;
+  }
+  if (req.body.started != null) {
+    res.user.started = req.body.started;
+  }
+  try {
+    const updatedUser = await res.user.save();
+    res.json(updatedUser);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+router.delete("/:id", getUsers, async (req, res) => {
+  try {
+    await res.user.delete();
+    res.json({ message: "Costumer removed" });
+  } catch (err) {
+    res.status(404).json({ message: "Costumer not found" });
+  }
+});
+
+async function getUsers(req, res, next) {
+  let user;
+  try {
+    user = await User.findById(req.params.id);
+  } catch (err) {
+    res.status(404).json({ message: err });
+  }
+
+  res.user = user;
+  next();
+}
+
+module.exports = router;
--- src/routes/plans.js
+++ src/routes/plans.js
@@ -1,5 +1,77 @@
-module.exports = (app) => {
-  app.get("/plans", (req, res) => {
-    res.send("route plans up");
+const express = require("express");
+const router = express.Router();
+const Plan = require("../models/plan");
+
+router.get("/", async (req, res) => {
+  try {
+    const plans = await Plan.find();
+    res.status(201).send(plans);
+  } catch (err) {
+    res.status(500).json({ message: err });
+  }
+});
+
+router.get("/:id", getPlans, (req, res) => {
+  res.send(res.plan);
+});
+
+router.post("/", async (req, res) => {
+  const plan = new Plan({
+    name: req.body.name,
+    price: req.body.price,
+    size: req.body.size,
+    screens: req.body.screens,
   });
-};
+
+  try {
+    const newPlan = await plan.save();
+    res.status(201).json(newPlan);
+  } catch (err) {
+    res.status(500).json({ message: err });
+  }
+});
+
+router.put("/:id", getPlans, async (req, res) => {
+  if (req.body.name != null) {
+    res.plan.name = req.body.name;
+  }
+  if (req.body.price != null) {
+    res.plan.price = req.body.price;
+  }
+  if (req.body.size != null) {
+    res.plan.size = req.body.size;
+  }
+  if (req.body.screens != null) {
+    res.plan.screens = req.body.screens;
+  }
+
+  try {
+    const updatedPlan = await res.plan.save();
+    res.json(updatedPlan);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+router.delete("/:id", getPlans, async (req, res) => {
+  try {
+    await res.plan.delete();
+    res.send({ message: "Plan removed" });
+  } catch (err) {
+    res.status(404).send({ message: err });
+  }
+});
+
+async function getPlans(req, res, next) {
+  let plan;
+  try {
+    plan = await Plan.findById(req.params.id);
+  } catch (err) {
+    res.status(404).send({ message: err });
+  }
+
+  res.plan = plan;
+  next();
+}
+
+module.exports = router;
